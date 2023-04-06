e1d7aaac718a6357fac4f965b410aebb04ea5764
FazedDark <106376154+FazedDark@users.noreply.github.com>
2023-04-06 17:21:34 +0545

    Add files via upload

---
+++ WeatherApp i guess/script.js
@@ -0,0 +1,50 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ed7722bbfdmsh1897eb738cff775p140243jsn40a309f79996',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+    
+};
+const getWeather = (city)=>{
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city= ' + city, options)
+    .then(response => response.json())
+    .then((response) => {
+
+
+
+        console.log(response)
+
+        cloud_pct.innerHTML = response.cloud_pct
+  
+        temp.innerHTML = response.temp   
+  
+        feels_like.innerHTML = response.feels_like
+  
+  
+        humidity.innerHTML = response.humidity
+  
+  
+        min_temp.innerHTML = response.min_temp
+ 
+        max_temp.innerHTML = response.max_temp
+  
+        wind_speed.innerHTML = response.wind_speed
+  
+  
+  sunrise.innerHTML  = response.sunrise 
+ 
+  
+  sunset.innerHTML  = response.sunset
+
+    })
+    .catch(err => console.error(err));
+}
+sumbit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+}
+)
+
+getWeather(["Kathmandu"])
